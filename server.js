a35c768df650145bce5d015adf7fae1c723a11cd
unknown <nvmaxx@gmail.com>
2022-08-24 22:32:30 -0500

    removed api and html js realized its not correct by looking at index.js in js folder

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3001;
+// need to remake these functions to use the new data structure
+const db = require('./db/db.json');
+const api = require('./routes/api');
+const html = require('./routes/html');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // use public/assets
-app.use(express.static('public/assets,'));
-require('./routes/html')(app);
-require('./routes/api')(app);
+app.use(express.static('public'));
+// use api routes
+app.use('/api', api);
+// use html routes
+app.use('/', html);
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 }).on('error', err => {
   console.log(err);
 });
+
+
